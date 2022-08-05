544475c973840fefb5bfe41949d6035b4afdba3b
andregodasi <andre.godasi@gmail.com>
2022-08-05 00:28:18 -0300

    feat: add tabView

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,9 @@
 import { GetServerSideProps } from 'next';
 import { parseCookies } from 'nookies';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import { getAPIClient } from '../services/axios';
-
+import { Tab } from '@headlessui/react';
 import MainContainer from '@/containers/MainContainer';
 import {
   ViewGridIcon as ViewGridIconSolid,
@@ -150,7 +150,7 @@ const files = [
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
-
+  const [selectedIndex, setSelectedIndex] = useState(0);
   useEffect(() => {
     // api.get('/users');
   }, []);
@@ -258,122 +258,140 @@ export default function Dashboard() {
                       </button>
                     </div>
                   </div>
-
-                  {/* Tabs */}
-                  <div className="mt-3 sm:mt-2">
-                    <div className="sm:hidden">
-                      <label htmlFor="tabs" className="sr-only">
-                        Select a tab
-                      </label>
-                      {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
-                      <select
-                        id="tabs"
-                        name="tabs"
-                        className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                        defaultValue="Recently Viewed"
-                      >
-                        <option>Eventos</option>
-                        <option>Comunidades</option>
-                        <option>Empresa</option>
-                      </select>
-                    </div>
-                    <div className="hidden sm:block">
-                      <div className="flex items-center border-b border-gray-200">
-                        <nav
-                          className="-mb-px flex flex-1 space-x-6 xl:space-x-8"
-                          aria-label="Tabs"
+                  <Tab.Group
+                    selectedIndex={selectedIndex}
+                    onChange={setSelectedIndex}
+                  >
+                    {/* Tabs */}
+                    <div className="mt-3 sm:mt-2">
+                      <div className="sm:hidden">
+                        <label htmlFor="tabs" className="sr-only">
+                          Select a tab
+                        </label>
+                        {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
+                        <select
+                          id="tabs"
+                          name="tabs"
+                          className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+                          defaultValue="Recently Viewed"
                         >
-                          {tabs.map((tab) => (
-                            <a
-                              key={tab.name}
-                              href={tab.href}
-                              aria-current={tab.current ? 'page' : undefined}
-                              className={classNames(
-                                tab.current
-                                  ? 'border-blue-500 text-blue-600'
-                                  : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                                'whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium'
-                              )}
+                          <option>Eventos</option>
+                          <option>Comunidades</option>
+                          <option>Empresa</option>
+                        </select>
+                      </div>
+                      <div className="hidden sm:block">
+                        <div className="flex items-center border-b border-gray-200">
+                          <nav
+                            className="-mb-px flex flex-1 space-x-6 xl:space-x-8"
+                            aria-label="Tabs"
+                          >
+                            {tabs.map((tab, index) => (
+                              <Tab
+                                className={classNames(
+                                  index === selectedIndex
+                                    ? 'border-blue-500 text-blue-600'
+                                    : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                                  'whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium'
+                                )}
+                              >
+                                <a
+                                  key={tab.name}
+                                  href={tab.href}
+                                  aria-current={
+                                    index === selectedIndex ? 'page' : undefined
+                                  }
+                                  className={classNames()}
+                                >
+                                  {tab.name}
+                                </a>
+                              </Tab>
+                            ))}
+                          </nav>
+                          <div className="ml-6 hidden items-center rounded-lg bg-gray-100 p-0.5 sm:flex">
+                            <button
+                              type="button"
+                              className="rounded-md p-1.5 text-gray-400 hover:bg-white hover:shadow-sm focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
                             >
-                              {tab.name}
-                            </a>
-                          ))}
-                        </nav>
-                        <div className="ml-6 hidden items-center rounded-lg bg-gray-100 p-0.5 sm:flex">
-                          <button
-                            type="button"
-                            className="rounded-md p-1.5 text-gray-400 hover:bg-white hover:shadow-sm focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
-                          >
-                            <ViewListIcon
-                              className="h-5 w-5"
-                              aria-hidden="true"
-                            />
-                            <span className="sr-only">Use list view</span>
-                          </button>
-                          <button
-                            type="button"
-                            className="ml-0.5 rounded-md bg-white p-1.5 text-gray-400 shadow-sm focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
-                          >
-                            <ViewGridIconSolid
-                              className="h-5 w-5"
-                              aria-hidden="true"
-                            />
-                            <span className="sr-only">Use grid view</span>
-                          </button>
+                              <ViewListIcon
+                                className="h-5 w-5"
+                                aria-hidden="true"
+                              />
+                              <span className="sr-only">Use list view</span>
+                            </button>
+                            <button
+                              type="button"
+                              className="ml-0.5 rounded-md bg-white p-1.5 text-gray-400 shadow-sm focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
+                            >
+                              <ViewGridIconSolid
+                                className="h-5 w-5"
+                                aria-hidden="true"
+                              />
+                              <span className="sr-only">Use grid view</span>
+                            </button>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
 
-                  {/* Gallery */}
-                  <section
-                    className="mt-8 pb-16"
-                    aria-labelledby="gallery-heading"
-                  >
-                    <h2 id="gallery-heading" className="sr-only">
-                      Recently viewed
-                    </h2>
-                    <ul
-                      role="list"
-                      className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"
-                    >
-                      {files.map((file) => (
-                        <li key={file.name} className="relative">
-                          <div
-                            className={classNames(
-                              file.current
-                                ? 'ring-2 ring-blue-500 ring-offset-2'
-                                : 'focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100',
-                              'aspect-w-10 aspect-h-7 group block w-full overflow-hidden rounded-lg bg-gray-100'
-                            )}
+                    {/* Gallery */}
+                    <Tab.Panels>
+                      <Tab.Panel>
+                        <section
+                          className="mt-8 pb-16"
+                          aria-labelledby="gallery-heading"
+                        >
+                          <h2 id="gallery-heading" className="sr-only">
+                            Recently viewed
+                          </h2>
+                          <ul
+                            role="list"
+                            className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"
                           >
-                            <img
-                              src={file.source}
-                              alt=""
-                              className={classNames(
-                                file.current ? '' : 'group-hover:opacity-75',
-                                'pointer-events-none object-cover'
-                              )}
-                            />
-                            <button
-                              type="button"
-                              className="absolute inset-0 focus:outline-none"
-                            >
-                              <span className="sr-only">
-                                View details for {file.name}
-                              </span>
-                            </button>
-                          </div>
-                          <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
-                            {file.name}
-                          </p>
-                          <p className="pointer-events-none block text-sm font-medium text-gray-500">
-                            {file.size}
-                          </p>
-                        </li>
-                      ))}
-                    </ul>
-                  </section>
+                            {files.map((file) => (
+                              <li key={file.name} className="relative">
+                                <div
+                                  className={classNames(
+                                    file.current
+                                      ? 'ring-2 ring-blue-500 ring-offset-2'
+                                      : 'focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100',
+                                    'aspect-w-10 aspect-h-7 group block w-full overflow-hidden rounded-lg bg-gray-100'
+                                  )}
+                                >
+                                  <img
+                                    src={file.source}
+                                    alt=""
+                                    className={classNames(
+                                      file.current
+                                        ? ''
+                                        : 'group-hover:opacity-75',
+                                      'pointer-events-none object-cover'
+                                    )}
+                                  />
+                                  <button
+                                    type="button"
+                                    className="absolute inset-0 focus:outline-none"
+                                  >
+                                    <span className="sr-only">
+                                      View details for {file.name}
+                                    </span>
+                                  </button>
+                                </div>
+                                <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
+                                  {file.name}
+                                </p>
+                                <p className="pointer-events-none block text-sm font-medium text-gray-500">
+                                  {file.size}
+                                </p>
+                              </li>
+                            ))}
+                          </ul>
+                        </section>
+                      </Tab.Panel>
+                      <Tab.Panel>Content 2</Tab.Panel>
+                      <Tab.Panel>Content 3</Tab.Panel>
+                    </Tab.Panels>
+                  </Tab.Group>
                 </div>
               </main>
             </div>
